7ff232f419789db65257453d43aa8b2d1db902dd
Juan in a million <59286923+damianjuan@users.noreply.github.com>
2020-07-13 22:34:18 -0500

    schema updated. api routes now working. total workout duration not working

--- models/WorkoutSchema.js
+++ models/WorkoutSchema.js
@@ -3,14 +3,40 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const WorkoutSchema = new Schema({
-    type: String,
-    name: String,
-    duration: Number,
-    weight: Number,
-    reps: Number,
-    sets: Number,
-    distance: Number
-});
+    exercises: [{
+        type: {
+            type: "String"
+        },
+        name: {
+            type: String
+        },
+        duration: {
+            type: Number
+        },
+        distance: {
+            type: Number
+        },
+        weight: {
+            type: Number
+        },
+        reps: {
+            type: Number
+        },
+        sets: {
+            type: Number
+        }
+    }],
+    day: {
+        type: Date,
+        default: Date.now
+    }
+
+},
+    {
+        toJSON: {
+            virtuals: true
+        }
+    });
 
 const Workout = mongoose.model("workout", WorkoutSchema);
 
